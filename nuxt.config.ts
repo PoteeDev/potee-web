5646e42209850c60e15dcc188d1516afe37df27e
Brynze Maxim <marimaxik123@gmail.com>
2022-10-11 02:18:21 +0300

    feat: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
+import { fileURLToPath } from 'url'
 
+export default defineNuxtConfig({
+  alias: {
+    'style': fileURLToPath(new URL('./assets/style', import.meta.url)),
+  },
+  modules: ['@nuxtjs/tailwindcss']
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    StartPage
-  </div>
+  <div class="underline">StartPage</div>
 </template>
 <script>
 export default {};
